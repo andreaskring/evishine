fd1d5e213e1256c055f4a1028d6780482ab38939
Andreas Kring <andreas@magenta.dk>
2017-03-26 11:26:25 +0200

    Ongoing work...

--- evishine.py
+++ evishine.py
@@ -1,5 +1,8 @@
 import requests
 
+# PANEL_ID = {1: 36976, 11: 37392, 13: 31487, 23: 31468, 25: 31470, }
+PANEL_ID = {1: 36976, 11: 37392}
+
 client = requests.session()
 
 res = client.get('https://evishine.dk/data/json_data/17249/37392')
